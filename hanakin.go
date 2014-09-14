6614cc26b700a35db6f5a8bc0e2bee76f4ed5fd2
Kentaro Kuribayashi <kentarok@gmail.com>
2014-09-15 02:18:47 +0900

    initial commit

---
+++ hanakin.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"time"
+)
+
+var y, m, p int
+
+func init() {
+	flag.IntVar(&y, "year", 2014, "The year you want to know about")
+	flag.IntVar(&m, "month", 9, "The month you want to know about")
+	flag.IntVar(&p, "payday", 10, "The payday of your company")
+}
+
+func main() {
+	flag.Parse()
+	now := time.Now()
+
+	if y == 0 {
+		y = now.Year()
+	}
+
+	if m != 0 {
+		if m < 1 || m > 12 {
+			log.Fatalf("month must been between 1 and 12: %s", m)
+		}
+	} else {
+		m = int(now.Month())
+	}
+
+	calendar := NewCalendar(time.Month(m), y, p)
+	fmt.Println(calendar.String())
+}
+
+type Calendar struct {
+	year  int
+	month *month
+}
+
+func NewCalendar(m time.Month, y, p int) (c *Calendar) {
+	c = &Calendar{year: y, month: NewMonth(m, y, p)}
+	return
+}
+
+func (c *Calendar) String() (s string) {
+	s = fmt.Sprintf("%d %s\n", c.year, c.month.number.String())
+	s += "====================\n"
+	s += c.month.String()
+	return
+}
+
+type month struct {
+	year   int
+	number time.Month
+	days   []time.Time
+	payday int
+}
+
+func NewMonth(number time.Month, y, p int) (m *month) {
+	m = &month{year: y, number: number, payday: p, days: make([]time.Time, 31)}
+	t := time.Date(y, number, 1, 0, 0, 0, 0, time.Local)
+
+	for {
+		m.days[t.Day() - 1] = t
+		t = time.Date(y, number, t.Day()+1, 0, 0, 0, 0, time.Local)
+
+		if t.Month().String() != number.String() {
+			break
+		}
+	}
+
+	return
+}
+
+func (m *month) String() (s string) {
+	for _, d := range m.days {
+		if d.Weekday().String() == "Friday" && d.Day() == m.payday {
+			s += "FizzBuzz\n"
+		} else if d.Weekday().String() == "Friday" {
+			s += "Fizz\n"
+		} else if d.Day() == m.payday {
+			s += "Buzz\n"
+		} else {
+			s += fmt.Sprintf("%d\n", d.Day())
+		}
+	}
+
+	return
+}
